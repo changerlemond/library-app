c5d0a72753a6ce6e720c9aef7a802af8e8c6942c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2023-01-27 20:28:04 +0900

    verify test code coverage

---
+++ src/test/kotlin/LibraryAppTestApplication.kt
@@ -0,0 +1,9 @@
+import org.springframework.boot.runApplication
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryAppTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibraryAppTestApplication>(*args)
+}

054c5e9b6d279d5e3b248faaa4f3c369c2e1ad35
changerlemond <83348294+changerlemond@users.noreply.github.com>
2024-04-26 21:22:09 +0900

    test

--- src/test/kotlin/LibraryAppTestApplication.kt
+++ src/test/kotlin/LibraryAppTestApplication.kt
@@ -3,7 +3,3 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class LibraryAppTestApplication
-
-fun main(args: Array<String>) {
-    runApplication<LibraryAppTestApplication>(*args)
-}
